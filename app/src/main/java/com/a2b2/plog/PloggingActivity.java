296e92a7f9cea97147eb48c2be9b3ce0e6d443ea
moonazn <sjy3729@naver.com>
2024-07-23 07:44:40 +0900

    0723 플로깅중 화면 구성완료

--- app/src/main/java/com/a2b2/plog/PloggingActivity.java
+++ app/src/main/java/com/a2b2/plog/PloggingActivity.java
@@ -1,14 +1,22 @@
 package com.a2b2.plog;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,33 +27,133 @@ public class PloggingActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private PloggerAdapter ploggerAdapter;
     private List<RealtimePloggerItem> ploggingItems;
+    private BottomSheetBehavior<LinearLayout> bottomSheetBehavior;
+    private ImageView topBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_plogging);
 
-        recyclerView = findViewById(R.id.ploggerRecyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        try{
+            setContentView(R.layout.activity_plogging);
 
-        // 이전 액티비티에서 전달된 데이터 받기
-        Intent intent = getIntent();
-        ArrayList<RealtimePloggerItem> ploggingItems = (ArrayList<RealtimePloggerItem>) intent.getSerializableExtra("ploggingItems");
+            // RecyclerView 초기화
+            recyclerView = findViewById(R.id.ploggerRecyclerView);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+            // 이전 액티비티에서 전달된 데이터 받기
+            Intent intent = getIntent();
+            ArrayList<RealtimePloggerItem> ploggingItems = (ArrayList<RealtimePloggerItem>) intent.getSerializableExtra("ploggingItems");
+
+            // Adapter 설정
+            ploggerAdapter = new PloggerAdapter(ploggingItems);
+            recyclerView.setAdapter(ploggerAdapter);
+
+            // 뒤로가기 버튼 초기화
+            backBtn = findViewById(R.id.backBtn);
+            backBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(PloggingActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    overridePendingTransition(0, 0);
+                    finish();
+                }
+            });
 
 
-        ploggerAdapter = new PloggerAdapter(ploggingItems);
-        recyclerView.setAdapter(ploggerAdapter);
+            // BottomSheet 초기화
+            LinearLayout bottomSheet = findViewById(R.id.bottom_sheet);
+            bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
 
-        backBtn = findViewById(R.id.backBtn);
+            // BottomSheet 초기 설정
+            bottomSheetBehavior.setPeekHeight(200); // 화면 하단에서 살짝 보이게 하는 높이 설정
+            bottomSheetBehavior.setHideable(true);  // 스와이프 시 BottomSheet가 숨겨지도록 설정
 
-        backBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(PloggingActivity.this, HomeActivity.class);
-                startActivity(intent);
-                overridePendingTransition(0, 0);
-                finish();
+            // 이미지뷰 클릭 리스너 설정
+            topBtn = findViewById(R.id.topBtn);
+            topBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (bottomSheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+                        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                        topBtn.setImageResource(R.drawable.bottom);
+                    } else {
+                        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                        topBtn.setImageResource(R.drawable.top);
+                    }
+                }
+            });
+
+            // CoordinatorLayout 클릭 리스너 설정
+            CoordinatorLayout coordinatorLayout = findViewById(R.id.coordinatorLayout);
+            coordinatorLayout.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (bottomSheetBehavior.getState() == BottomSheetBehavior.STATE_EXPANDED) {
+                        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                        topBtn.setImageResource(R.drawable.top);
+
+                        return true;
+                    }
+                    return false;
+                }
+            });
+
+            // BottomSheet 상태 변경 시 로그 출력
+            bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+                @Override
+                public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                    // BottomSheetBehavior state에 따른 이벤트
+                    switch (newState) {
+                        case BottomSheetBehavior.STATE_HIDDEN:
+                            Log.d("PloggingActivity", "state: hidden");
+                            break;
+                        case BottomSheetBehavior.STATE_EXPANDED:
+                            topBtn.setImageResource(R.drawable.bottom);
+
+                            Log.d("PloggingActivity", "state: expanded");
+                            break;
+                        case BottomSheetBehavior.STATE_COLLAPSED:
+                            topBtn.setImageResource(R.drawable.top);
+
+                            Log.d("PloggingActivity", "state: collapsed");
+                            break;
+                        case BottomSheetBehavior.STATE_DRAGGING:
+                            Log.d("PloggingActivity", "state: dragging");
+                            break;
+                        case BottomSheetBehavior.STATE_SETTLING:
+                            Log.d("PloggingActivity", "state: settling");
+                            break;
+                        case BottomSheetBehavior.STATE_HALF_EXPANDED:
+                            Log.d("PloggingActivity", "state: half expanded");
+                            break;
+                    }
+                }
+
+                @Override
+                public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                    // Handle slide offset if needed
+                }
+            });
+
+
+            LinearLayout trashContainer = findViewById(R.id.trashContainer);
+
+            String[] trashTypes = {"일반쓰레기", "플라스틱", "종이류", "캔/고철류", "병류", "비닐류"};
+
+            LayoutInflater inflater = LayoutInflater.from(this);
+
+            for (String trashType : trashTypes) {
+                View itemView = inflater.inflate(R.layout.item_trash, trashContainer, false);
+                TextView tvTrashType = itemView.findViewById(R.id.trashType);
+                tvTrashType.setText(trashType);
+                trashContainer.addView(itemView);
             }
-        });
+        }catch (Exception e) {
+            Log.e("PloggingActivity", "Exception in onCreate", e);
+
+        }
+
     }
 }
