e923ef309e8b241e68cef0414c49d7d332114bde
moonazn <sjy3729@naver.com>
2024-07-23 02:04:01 +0900

    0723 미션페이지 퀘스트 구성

--- app/src/main/java/com/a2b2/plog/CommunityActivity.java
+++ app/src/main/java/com/a2b2/plog/CommunityActivity.java
@@ -2,13 +2,61 @@ package com.a2b2.plog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class CommunityActivity extends AppCompatActivity {
+
+    private ImageView rank, home, mission, mypage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community);
+
+        rank = findViewById(R.id.rank);
+        mission = findViewById(R.id.mission);
+        home = findViewById(R.id.home);
+        mypage = findViewById(R.id.mypage);
+
+        rank.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CommunityActivity.this, RankActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+                finish();
+            }
+        });
+
+        mission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CommunityActivity.this, MissionActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+                finish();
+            }
+        });
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CommunityActivity.this, HomeActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+                finish();
+            }
+        });
+        mypage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CommunityActivity.this, MyPageActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/a2b2/plog/MissionActivity.java
+++ app/src/main/java/com/a2b2/plog/MissionActivity.java
@@ -1,14 +1,95 @@
 package com.a2b2.plog;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.PagerSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MissionActivity extends AppCompatActivity {
+
+    private RecyclerView questRecyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<QuestItem> questList;
+    private ImageView rank, community, home, mypage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mission);
+
+        questList = new ArrayList<>();
+        // 여기에 퀘스트 아이템을 추가합니다.
+        questList.add(new QuestItem("플로깅 3KM 하기", 3));
+        questList.add(new QuestItem("플라스틱 10개 줍기", 2));
+        questList.add(new QuestItem("그룹 플로깅 1회 하기", 5));
+        questList.add(new QuestItem("그룹 플로깅 1회 하기", 5));
+        questList.add(new QuestItem("그룹 플로깅 1회 하기", 5));
+        questList.add(new QuestItem("그룹 플로깅 1회 하기", 5));
+
+        questRecyclerView = findViewById(R.id.questRecyclerView);
+        questRecyclerView.setHasFixedSize(true);
+        layoutManager = new GridLayoutManager(this, 3, GridLayoutManager.HORIZONTAL, false);
+        questRecyclerView.setLayoutManager(layoutManager);
+        adapter = new QuestAdapter(questList);
+        questRecyclerView.setAdapter(adapter);
+
+        // PagerSnapHelper를 사용하여 페이지 넘기기 기능을 추가합니다.
+        PagerSnapHelper snapHelper = new PagerSnapHelper();
+        snapHelper.attachToRecyclerView(questRecyclerView);
+
+        rank = findViewById(R.id.rank);
+        community = findViewById(R.id.community);
+        home = findViewById(R.id.home);
+        mypage = findViewById(R.id.mypage);
+
+        rank.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MissionActivity.this, RankActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+                finish();
+            }
+        });
+
+        community.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MissionActivity.this, CommunityActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+                finish();
+            }
+        });
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MissionActivity.this, HomeActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+                finish();
+            }
+        });
+        mypage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MissionActivity.this, MyPageActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+                finish();
+            }
+        });
+
     }
 }
