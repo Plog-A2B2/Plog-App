dc2900f8a3204b9d28c4a932f0a0f434dd119dd8
moonazn <sjy3729@naver.com>
2024-09-03 21:14:34 +0900

    0903 메인 실시간 플로거 통신, BadgeManager 구현

--- app/src/main/java/com/a2b2/plog/HomeActivity.java
+++ app/src/main/java/com/a2b2/plog/HomeActivity.java
@@ -9,15 +9,34 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonParser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.UUID;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -33,6 +52,7 @@ public class HomeActivity extends AppCompatActivity {
     int trashGoal = 0;
     private Button plggingStartBut;
     private SharedPreferencesHelper prefsHelper;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,14 +64,27 @@ public class HomeActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.ploggerRecyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
-        ploggingItems = new ArrayList<>();
-        // 샘플 데이터 추가
-        ploggingItems.add(new RealtimePloggerItem(R.drawable.badgetest, "nickname"));
-        ploggingItems.add(new RealtimePloggerItem(R.drawable.badgetest, "nickname"));
-        ploggingItems.add(new RealtimePloggerItem(R.drawable.badgetest, "nickname"));
-        ploggingItems.add(new RealtimePloggerItem(R.drawable.badgetest, "nickname"));
-        ploggingItems.add(new RealtimePloggerItem(R.drawable.badgetest, "nickname"));
-        ploggingItems.add(new RealtimePloggerItem(R.drawable.badgetest, "nickname"));
+        handler = new Handler();
+
+        String url = "http://15.164.152.246:8080/profile/active";
+        UUID uuid = UUID.fromString("57F7C28F-67A6-4091-B837-8C3168653B81");
+
+        new Thread(() -> {
+            String result = httpGetConnection(url, "");
+            ploggingItems = parseCommunityList(result);
+            // 처리 결과 확인
+            handler.post(() -> seeNetworkResult(result));
+        }).start();
+
+
+//        ploggingItems = new ArrayList<>();
+//        // 샘플 데이터 추가
+//        ploggingItems.add(new RealtimePloggerItem(R.drawable.badgetest, "nickname"));
+//        ploggingItems.add(new RealtimePloggerItem(R.drawable.badgetest, "nickname"));
+//        ploggingItems.add(new RealtimePloggerItem(R.drawable.badgetest, "nickname"));
+//        ploggingItems.add(new RealtimePloggerItem(R.drawable.badgetest, "nickname"));
+//        ploggingItems.add(new RealtimePloggerItem(R.drawable.badgetest, "nickname"));
+//        ploggingItems.add(new RealtimePloggerItem(R.drawable.badgetest, "nickname"));
 
 
         ploggerAdapter = new PloggerAdapter(ploggingItems);
@@ -142,6 +175,109 @@ public class HomeActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    public String httpGetConnection(String UrlData, String s) {
+        String totalUrl = UrlData.trim();
+
+        //http 통신을 하기위한 객체 선언 실시
+        URL url = null;
+        HttpURLConnection conn = null;
+
+        //http 통신 요청 후 응답 받은 데이터를 담기 위한 변수
+        String responseData = "";
+        BufferedReader br = null;
+        StringBuffer sb = null;
+
+        //메소드 호출 결과값을 반환하기 위한 변수
+        String returnData = "";
+
+        try {
+            //파라미터로 들어온 url을 사용해 connection 실시
+            url = new URL(totalUrl);
+            conn = (HttpURLConnection) url.openConnection();
+
+            //http 요청에 필요한 타입 정의 실시
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json; utf-8");
+
+            //http 요청 실시
+            conn.connect();
+            System.out.println("http 요청 방식 : " + "GET");
+            System.out.println("http 요청 주소 : " + totalUrl);
+            System.out.println("");
+
+            //http 요청 후 응답 받은 데이터를 버퍼에 쌓는다
+            br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            sb = new StringBuffer();
+            while ((responseData = br.readLine()) != null) {
+                sb.append(responseData); //StringBuffer에 응답받은 데이터 순차적으로 저장 실시
+            }
+
+            //메소드 호출 완료 시 반환하는 변수에 버퍼 데이터 삽입 실시
+            returnData = sb.toString();
+            Log.d("TAG2", returnData);
+            //http 요청 응답 코드 확인 실시
+            String responseCode = String.valueOf(conn.getResponseCode());
+            System.out.println("http 응답 코드 : " + responseCode);
+            System.out.println("http 응답 데이터 : " + returnData);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //http 요청 및 응답 완료 후 BufferedReader를 닫아줍니다
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return returnData; // 네트워크 요청 결과를 반환
+    }
+    public void seeNetworkResult(String result) {
+        // 네트워크 작업 완료 후
+        Log.d(result, "network");
+    }
+    public ArrayList<RealtimePloggerItem> parseCommunityList(String json) {
+        ArrayList<RealtimePloggerItem> ploggerList = new ArrayList<>();
+
+        try {
+            // JSON 객체를 파싱하여 "data" 배열을 가져옵니다.
+            JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
+            JsonArray jsonArray = jsonObject.getAsJsonArray("data");
+
+            // "data" 배열을 순회하며 각 객체를 파싱합니다.
+            for (int i = 0; i < jsonArray.size(); i++) {
+                JsonObject itemObject = jsonArray.get(i).getAsJsonObject();
+
+                String userNickname = null;
+                int badgeId = 0;
+
+                // JSON 객체에서 필요한 값을 추출합니다.
+                if (itemObject.has("userNickname")) {
+                    userNickname = itemObject.get("userNickname").getAsString();
+                    Log.d("userNickname", userNickname);
+                }
+
+                if (itemObject.has("badgeId")) {
+                    badgeId = itemObject.get("badgeId").getAsInt();
+                    Log.d("badgeId", String.valueOf(badgeId));
+                }
+                BadgeManager badgeManager = new BadgeManager();
+                // 모든 필드가 존재할 경우 리스트에 추가합니다.
+                if (userNickname != null) {
+                    RealtimePloggerItem ploggerItem = new RealtimePloggerItem(badgeManager.getDrawableForBadgeId(badgeId), userNickname);
+                    ploggerList.add(ploggerItem);
+                } else {
+                    Log.e("JSONError", "Missing key in JSON object: " + itemObject.toString());
+                }
+            }
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        }
+        return ploggerList;
     }
 
 }
