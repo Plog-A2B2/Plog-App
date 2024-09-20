f58cd91c753f3593387a915f280414598fa7a219
moonazn <sjy3729@naver.com>
2024-09-20 15:47:23 +0900

    0920 마이페이지 데이터 전부 받아서 표시 완료

--- app/src/main/java/com/a2b2/plog/MissionActivity.java
+++ app/src/main/java/com/a2b2/plog/MissionActivity.java
@@ -129,7 +129,7 @@ public class MissionActivity extends AppCompatActivity {
         String data = "";
 
         new Thread(() -> {
-            result = httpPostBodyConnection(url, data);
+            result = httpPostBodyConnection2(url, data);
             handler.post(() -> {
                 if (result != null && !result.isEmpty()) {
                     myBadgeItemList = parseBadgeAll(result);
@@ -266,6 +266,87 @@ public class MissionActivity extends AppCompatActivity {
     }
 
     public String httpPostBodyConnection(String UrlData, String ParamData) {
+        // 이전과 동일한 네트워크 연결 코드를 그대로 사용합니다.
+        // 백그라운드 스레드에서 실행되기 때문에 메인 스레드에서는 문제가 없습니다.
+
+        String totalUrl = "";
+        totalUrl = UrlData.trim().toString();
+
+        //http 통신을 하기위한 객체 선언 실시
+        URL url = null;
+        HttpURLConnection conn = null;
+
+        //http 통신 요청 후 응답 받은 데이터를 담기 위한 변수
+        String responseData = "";
+        BufferedReader br = null;
+        StringBuffer sb = null;
+
+        //메소드 호출 결과값을 반환하기 위한 변수
+        String returnData = "";
+
+
+        try {
+            //파라미터로 들어온 url을 사용해 connection 실시
+            url = null;
+            url = new URL(totalUrl);
+            conn = null;
+            conn = (HttpURLConnection) url.openConnection();
+
+            //http 요청에 필요한 타입 정의 실시
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json; utf-8"); //post body json으로 던지기 위함
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setDoOutput(true); //OutputStream을 사용해서 post body 데이터 전송
+            try (OutputStream os = conn.getOutputStream()) {
+                byte request_data[] = ParamData.getBytes("utf-8");
+                Log.d("TAGGG",request_data.toString());
+                os.write(request_data);
+                //os.close();
+            } catch (Exception e) {
+                Log.d("TAG3","여기다");
+                e.printStackTrace();
+            }
+
+            //http 요청 실시
+            conn.connect();
+            System.out.println("http 요청 방식 : " + "POST BODY JSON");
+            System.out.println("http 요청 타입 : " + "application/json");
+            System.out.println("http 요청 주소 : " + UrlData);
+            System.out.println("http 요청 데이터 : " + ParamData);
+            System.out.println("");
+
+            //http 요청 후 응답 받은 데이터를 버퍼에 쌓는다
+            br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            sb = new StringBuffer();
+            while ((responseData = br.readLine()) != null) {
+                sb.append(responseData); //StringBuffer에 응답받은 데이터 순차적으로 저장 실시
+            }
+
+            //메소드 호출 완료 시 반환하는 변수에 버퍼 데이터 삽입 실시
+            returnData = sb.toString();
+            Log.d("TAG2", returnData);
+            //http 요청 응답 코드 확인 실시
+            String responseCode = String.valueOf(conn.getResponseCode());
+            System.out.println("http 응답 코드 : " + responseCode);
+            System.out.println("http 응답 데이터 : " + returnData);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //http 요청 및 응답 완료 후 BufferedReader를 닫아줍니다
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return returnData; // 네트워크 요청 결과를 반환
+    }
+
+    public String httpPostBodyConnection2(String UrlData, String ParamData) {
         // 이전과 동일한 네트워크 연결 코드를 그대로 사용합니다.
         // 백그라운드 스레드에서 실행되기 때문에 메인 스레드에서는 문제가 없습니다.
 
--- app/src/main/java/com/a2b2/plog/MyPageActivity.java
+++ app/src/main/java/com/a2b2/plog/MyPageActivity.java
@@ -27,9 +27,11 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.kakao.sdk.user.model.User;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -65,6 +67,8 @@ public class MyPageActivity extends AppCompatActivity {
     private ImageView coinInfoBtn, profileImg;
 
     private ImageView membership;
+    String url2, result2;
+    TextView coinNumTextView, nicknameTextView, distanceTextView, timeTextView, trashNumTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -82,6 +86,11 @@ public class MyPageActivity extends AppCompatActivity {
         coinInfoBtn = findViewById(R.id.coinInfoBtn);
         profileImg = findViewById(R.id.profileImg);
         membership = findViewById(R.id.membership);
+        coinNumTextView = findViewById(R.id.coinNumTextView);
+        nicknameTextView = findViewById(R.id.nicknameTextView);
+        distanceTextView = findViewById(R.id.distanceTextView);
+        timeTextView = findViewById(R.id.timeTextView);
+        trashNumTextView = findViewById(R.id.trashNumTextView);
 
         membership.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -120,6 +129,7 @@ public class MyPageActivity extends AppCompatActivity {
         Log.d("주소 확인",url);
         new Thread(()->{
             String result = httpGetConnection(url, "");
+            Log.d("result", result);
 
             if (handler != null) {
                 handler.post(() -> {
@@ -156,12 +166,63 @@ public class MyPageActivity extends AppCompatActivity {
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
-                    seeNetworkResult(result);
                 });
             }
         }).start();
 
-        profileImg.setImageResource(BadgeManager.getDrawableForBadgeId(userManager.getBadgeId()));
+        url2 = "http://15.164.152.246:8080/profile/"+uuid+"/MyPageInformation";
+        Log.d("주소 확인",url2);
+        new Thread(()->{
+            String result2 = httpGetConnection2(url2, "");
+
+            if (handler != null) {
+                handler.post(() -> {
+                    try {
+                        // JSON 객체 생성
+                        JSONObject jsonObject = new JSONObject(result2);
+
+                        // "message" 값 가져오기
+                        String message = jsonObject.getString("message");
+                        System.out.println("Message: " + message);
+
+                        // "data" 객체 가져오기
+                        JSONObject dataObject = jsonObject.getJSONObject("data");
+
+                        // 각 데이터 필드 파싱
+                        String userNickname = dataObject.getString("userNickname");
+                        int totalCoin = dataObject.getInt("totalCoin");
+                        int badge = dataObject.getInt("badge");
+                        double totalDistance = dataObject.getDouble("totalDistance");
+                        int totalTime = dataObject.getInt("totalTime");
+                        int totalTrash = dataObject.getInt("totalTrash");
+
+                        // 결과 출력
+                        System.out.println("User Nickname: " + userNickname);
+                        System.out.println("Total Coin: " + totalCoin);
+                        System.out.println("Badge: " + badge);
+                        System.out.println("Total Distance: " + totalDistance);
+                        System.out.println("Total Time: " + totalTime);
+                        System.out.println("Total Trash: " + totalTrash);
+
+                        coinNumTextView.setText(totalCoin + "코인");
+                        UserManager.getInstance().setBadgeId(badge);
+                        profileImg.setImageResource(BadgeManager.getDrawableForBadgeId(userManager.getBadgeId()));
+                        UserManager.getInstance().setUserNickname(userNickname);
+                        nicknameTextView.setText(userNickname + " 님");
+
+                        distanceTextView.setText(totalDistance + " KM");
+                        int hours = totalTime / 3600;        // 시간 계산 (1시간 = 3600초)
+                        timeTextView.setText(hours + " H");
+                        trashNumTextView.setText(totalTrash + " 개");
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    seeNetworkResult(result2);
+                });
+            }
+        }).start();
+
 //        profileImg.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
@@ -300,6 +361,8 @@ public class MyPageActivity extends AppCompatActivity {
                 Log.d("주소 확인",url);
                 new Thread(()->{
                     if(standardLocation.getAddress() != null) {
+                        Log.d("if문 안에들어왓다", standardLocation.getAddress());
+
                         getGeoDataByAddress(standardLocation.getAddress());
                     }
 
@@ -394,7 +457,22 @@ public class MyPageActivity extends AppCompatActivity {
                         public void onClick(DialogInterface dialog, int which) {
                             // "예" 버튼 클릭 시의 행동
                             //멤버십 해지
-                            UserManager.getInstance().setMembership(false);
+                            UUID uuid = UserManager.getInstance().getUserId();
+
+                            String url = "http://15.164.152.246:8080/profile/" + uuid + "/membershipcancel";
+                            new Thread(() -> {
+                                String result = httpPostBodyConnection(url, "");
+                                // 처리 결과 확인
+                                handler = new Handler(Looper.getMainLooper());
+                                if (handler != null) {
+                                    handler.post(() -> {
+                                        Log.d("result", result);
+                                        Toast.makeText(MyPageActivity.this, "멤버십이 해지되었습니다.", Toast.LENGTH_LONG).show();
+                                        UserManager.getInstance().setMembership(false);
+                                    });
+
+                                }
+                            }).start();
                         }
                     })
                     .setNegativeButton("아니요", new DialogInterface.OnClickListener() {
@@ -532,46 +610,60 @@ public class MyPageActivity extends AppCompatActivity {
 
     private void getGeoDataByAddress(String completeAddress) {
         try {
-            String API_KEY = "AIzaSyC6FB54gjhzW2wfkKqD8vo5OybyxW55k8M";
+            String API_KEY = "AIzaSyCD4wiVWqJJAq1ipj5VdS4CXVG7ulEswkE";
             String surl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + URLEncoder.encode(completeAddress, "UTF-8") + "&key=" + API_KEY;
             URL url = new URL(surl);
             InputStream is = url.openConnection().getInputStream();
 
             BufferedReader streamReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-
             StringBuilder responseStrBuilder = new StringBuilder();
             String inputStr;
             while ((inputStr = streamReader.readLine()) != null) {
                 responseStrBuilder.append(inputStr);
             }
 
+            // JSON 응답 파싱
             JSONObject jo = new JSONObject(responseStrBuilder.toString());
+
+            // API 응답 상태 확인
+            String status = jo.getString("status");
+            if (!"OK".equals(status)) {
+                System.err.println("Geocoding API request failed. Status: " + status);
+                return;
+            }
+
             JSONArray results = jo.getJSONArray("results");
-            String region = null;
-            String province = null;
-            String zip = null;
             if (results.length() > 0) {
-                JSONObject jsonObject;
-                jsonObject = results.getJSONObject(0);
+                JSONObject jsonObject = results.getJSONObject(0);
                 Double lat = jsonObject.getJSONObject("geometry").getJSONObject("location").getDouble("lat");
                 Double lng = jsonObject.getJSONObject("geometry").getJSONObject("location").getDouble("lng");
-//                ret.put(lat);
-//                ret.put(lng);
+
+                // 위도와 경도 설정
                 latitude = lat;
                 longtitude = lng;
 
                 standardLocation.setLatitude(latitude);
                 standardLocation.setLongitude(longtitude);
 
-                System.out.println("LAT:\t\t" + lat);
-                System.out.println("LNG:\t\t" + lng);
-                JSONArray ja = jsonObject.getJSONArray("address_components");
-
+                System.out.println("LAT: " + lat);
+                System.out.println("LNG: " + lng);
+            } else {
+                System.err.println("No results found for the given address.");
             }
-        } catch(Exception e){
+        } catch (FileNotFoundException e) {
+            System.err.println("Geocoding API URL not found. Check the API key or URL.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("I/O error while connecting to the Geocoding API.");
+            e.printStackTrace();
+        } catch (JSONException e) {
+            System.err.println("Error parsing JSON response from Geocoding API.");
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
     public String httpPostBodyConnection(String UrlData, String ParamData) {
         // 이전과 동일한 네트워크 연결 코드를 그대로 사용합니다.
         // 백그라운드 스레드에서 실행되기 때문에 메인 스레드에서는 문제가 없습니다.
@@ -659,6 +751,66 @@ public class MyPageActivity extends AppCompatActivity {
 //        Log.d("uuid",userUUIDStr);
     }
     public String httpGetConnection(String UrlData, String s) {
+        String totalUrl = UrlData.trim();
+
+        //http 통신을 하기위한 객체 선언 실시
+        URL url = null;
+        HttpURLConnection conn = null;
+
+        //http 통신 요청 후 응답 받은 데이터를 담기 위한 변수
+        String responseData = "";
+        BufferedReader br = null;
+        StringBuffer sb = null;
+
+        //메소드 호출 결과값을 반환하기 위한 변수
+        String returnData = "";
+
+        try {
+            //파라미터로 들어온 url을 사용해 connection 실시
+            url = new URL(totalUrl);
+            conn = (HttpURLConnection) url.openConnection();
+
+            //http 요청에 필요한 타입 정의 실시
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json; utf-8");
+
+            //http 요청 실시
+            conn.connect();
+            System.out.println("http 요청 방식 : " + "GET");
+            System.out.println("http 요청 주소 : " + totalUrl);
+            System.out.println("");
+
+            //http 요청 후 응답 받은 데이터를 버퍼에 쌓는다
+            br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            sb = new StringBuffer();
+            while ((responseData = br.readLine()) != null) {
+                sb.append(responseData); //StringBuffer에 응답받은 데이터 순차적으로 저장 실시
+            }
+
+            //메소드 호출 완료 시 반환하는 변수에 버퍼 데이터 삽입 실시
+            returnData = sb.toString();
+            Log.d("TAG2", returnData);
+            //http 요청 응답 코드 확인 실시
+            String responseCode = String.valueOf(conn.getResponseCode());
+            System.out.println("http 응답 코드 : " + responseCode);
+            System.out.println("http 응답 데이터 : " + returnData);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //http 요청 및 응답 완료 후 BufferedReader를 닫아줍니다
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return returnData; // 네트워크 요청 결과를 반환
+    }
+    public String httpGetConnection2(String UrlData, String s) {
         String totalUrl = UrlData.trim();
 
         //http 통신을 하기위한 객체 선언 실시
